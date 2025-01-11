de24750248ffe33fe4453d429f4b1fa6b41ece4d
gotax24 <124403304+gotax24@users.noreply.github.com>
2025-01-10 20:58:08 -0400

    iniciando el proyecto

---
+++ src/components/Square.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const ClassName = `square ${isSelected ? "is-selected" : ""}`;
+
+  const handleClick = () => {
+    updateBoard(index);
+  };
+
+  return (
+    <div onClick={handleClick} className={ClassName}>
+      {children}
+    </div>
+  );
+};
+
+Square.propTypes = {
+  children: PropTypes.node,
+  isSelected: PropTypes.bool,
+  updateBoard: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired,
+};
+
+export default Square;
